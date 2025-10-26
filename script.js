aa38dfa345125f7673aa5206d20a6f128d1dadbf
shaikmoinaasim-24 <shaikmoinaasim@gmail.com>
2025-10-26 20:01:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Navbar toggle
+const menu = document.querySelector("#menu");
+const navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    navbar.classList.toggle("active");
+    menu.classList.toggle("bx-x");
+};
+
+// Scroll spy
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => link.classList.remove("active"));
+            const activeLink = document.querySelector(header nav a[href *= "${id}"]);
+            if (activeLink) activeLink.classList.add("active");
+        }
+    });
+};
